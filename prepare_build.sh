0c78e42251da8af240555ba7a72507eae7290bb0
Michael Herren <michherren@gmail.com>
2022-01-30 14:38:36 +0100

    feat: added script to update kowl version automatically

--- prepare_build.sh
+++
@@ -1,4 +0,0 @@
-docker build -t waldkauz-builder:latest .
-docker run --rm  waldkauz-desktop-builder bash
-docker cp  waldkauz-desktop-builder:/app/bin/ bin
-docker cp  waldkauz-desktop-builder:/app/waldkauz-data-template/frontend waldkauz-data-template/frontend
